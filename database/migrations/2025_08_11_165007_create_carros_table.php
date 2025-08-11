bbb218654969b2ccdfcae718e88918d67da33af9
Rafael Augusto <rafael.appdevs@gmail.com>
2025-08-11 14:17:21 -0300

    adiciona migrações para gerenciamento de 'Locacao', 'Marca', 'Modelo' e 'Carro'; cria diagrama do banco de dados

--- database/migrations/2025_08_11_165007_create_carros_table.php
+++ database/migrations/2025_08_11_165007_create_carros_table.php
@@ -9,19 +9,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
             $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('modelo_id')->references('id')->on('modelos');
         });
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
+        Schema::table('carros', function (Blueprint $table) {
+            $table->dropForeign(['modelo_id']);
+        });
         Schema::dropIfExists('carros');
     }
 };
