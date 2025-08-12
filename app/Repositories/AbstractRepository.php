42654650af512ae543ddd76282ac5613771a312e
Rafael Augusto <rafael.appdevs@gmail.com>
2025-08-12 16:58:49 -0300

    refactor: substitui implementações de MarcaRepository e ModeloRepository para herdar de AbstractRepository

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository {
+
+    protected $model;
+    private $colunasPermitidas;
+
+    public function __construct(Model $model){
+        $this->model = $model;
+        $this->colunasPermitidas = $model->getFillable();
+    }
+
+    public function selectAtributosRegistrosRelacionados(string $tabela, string $identificador, $atributos = null)
+    {
+        if($atributos === null){
+            $this->model = $this->model->with(''.$tabela.':'.$identificador);
+        }
+
+        $atributos = array_map('trim', explode(',', $atributos));
+
+        $this->model = $this->model->with(''.$tabela.':'.$identificador.',' . implode(',', $atributos));
+    }
+
+    public function selectAtributos(string $atributos)
+    {
+            $atributos = array_map('trim',  explode(',', $atributos));
+
+        $this->model = $this->model->select($atributos ?? $this->colunasPermitidas);
+    }
+
+    public function filtrarRegistros(string $filtro)
+    {
+         $separarMultiplosFiltros = explode(';', $filtro);
+
+        foreach ($separarMultiplosFiltros as $filtro) {
+            $separar = explode(':', $filtro);
+
+            $coluna = trim($separar[0]);
+            $operador = trim($separar[1]);
+            $valor = $separar[2];
+
+            if(!in_array($coluna, $this->colunasPermitidas)
+                OR !in_array($operador, ['=', '!=', '>', '<', '>=', '<=', 'like'])
+                OR is_null($valor)
+            ) {
+                return response()->json(['error' => 'Filtro inválido.'], 400);
+            } else {
+                $this->model = $this->model->where($coluna, $operador, $valor);
+            }
+        }
+    }
+
+    public function getResult(){
+        return $this->model->get();
+    }
+
+}
